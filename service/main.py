517f48d4abc806049ffdaecf287006d3de746a84
Qiacheng Li <qiacheng.li@intel.com>
2024-07-22 16:27:32 -0700

    Update main.py

--- service/main.py
+++ service/main.py
@@ -55,38 +55,3 @@ if __name__ == "__main__":
     for stream_output in streamer:
         print(stream_output, end="")
     print()
-
-
-# if __name__ == "__main__":
-#     from transformers import AutoTokenizer, AutoModelForCausalLM
-
-#     start = time.time()
-#     model_path = os.path.abspath("microsoft/Phi-3-mini-4k-instruct")
-#     model : PreTrainedModel | TFPreTrainedModel = AutoModelForCausalLM.from_pretrained(model_path)
-#     model = model.eval()
-#     end = time.time()
-#     print(f"load model cost: {(end-start)}s")
-#     model = model.to('xpu')
-#     start = time.time()
-#     model = ipex.optimize(model, dtype=torch.int8)
-#     end = time.time()
-#     print(f"ipex.optimize cost: {(end-start)}s")
-#     tokenizer = AutoTokenizer.from_pretrained(model_path)
-#     messages = [
-#         {
-#             "role": "system",
-#             "content": "You are a friendly chatbot who always responds in the style of a pirate",
-#         },
-#         {"role": "user", "content": "How many helicopters can a human eat in one sitting?"},
-#     ]
-#     prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-#     encoding = tokenizer.encode_plus(prompt,return_tensors="pt").to("xpu")
-#     tensor : torch.Tensor = encoding.get("input_ids")
-#     with torch.no_grad():
-#         with torch.xpu.amp.autocast(enabled=True, dtype=torch.bfloat16, cache_enabled=True):
-#             start = time.time()
-#             outputs = model.generate(inputs=tensor, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
-#             end = time.time()
-#             print(f"generate cost: {(end-start)}s")
-#             generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-#             print(generated_text)
