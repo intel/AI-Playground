e0adad2aa2f0251a614df993c57977f8f1922518
Markus Schuettler <markus.schuettler@tngtech.com>
2024-08-13 10:30:06 +0200

    Prevent LLM picker circles from becoming elliptical

--- WebUI/src/components/ModelDropDownItem.vue
+++ WebUI/src/components/ModelDropDownItem.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex gap-2 items-center overflow-hidden text-ellipsis">
-        <span class="rounded-full w-2 h-2"  :class="{ 'bg-green-500': model.downloaded, 'bg-gray-500': !model.downloaded }" ></span>
+        <span class="rounded-full w-2 h-2 flex-shrink-0"  :class="{ 'bg-green-500': model.downloaded, 'bg-gray-500': !model.downloaded }" ></span>
         <span class="h-7 overflow-hidden">{{ model.name }}</span>
     </div>
 </template>
