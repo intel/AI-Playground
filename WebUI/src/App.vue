2bc691f76df36e2ddbc4118397bb1db324c5fc73
Daniel Hirsch <daniel.hirsch@tngtech.com>
2024-08-20 15:19:53 +0100

    Add Collapsible for displaying error message to user (#8)

--- WebUI/src/App.vue
+++ WebUI/src/App.vue
@@ -24,13 +24,23 @@
     <loading-bar :text="'AI Playground Loading'" class="w-3/5" style="word-spacing: 8px;"></loading-bar>
   </main>
   <main v-else-if="globalSetup.loadingState === 'failed'" class="flex-auto flex items-center justify-center">
-    <div class="dialog-container z-10">
-        <div class="dialog-mask absolute left-0 top-0 w-full h-full bg-black/55 flex justify-center items-center">
-            <div class="py-20 px-20 w-768px flex flex-col items-center justify-center bg-gray-600 rounded-3xl gap-8 text-white">
-                Backend initialization failed. Please check the following reasons:
-                <pre>{{ globalSetup.errorMessage }}</pre>
-            </div>
-        </div>
+    <div class="dialog-container z-10 text-white">
+        <Collapsible v-model:open="isOpen" class="w-[600px] space-y-2">
+          <div class="flex items-center justify-between space-x-4 px-4">
+            <h2 class="text-base font-semibold text-red-600"> <!-- added error color -->
+              Backend initialization failed.
+            </h2>
+            <CollapsibleTrigger>
+              <button variant="default" size="sm" class="bg-gray-100 hover:bg-gray-200 text-gray-800 font-semibold py-1 px-2 border border-gray-400 rounded">
+                <i class="fas fa-chevron-down" /> 
+                Details
+              </button>
+            </CollapsibleTrigger>
+          </div>
+          <CollapsibleContent class="px-4 py-2">
+            <pre class="whitespace-pre-wrap">{{ globalSetup.errorMessage }}</pre>
+          </CollapsibleContent>
+        </Collapsible>
     </div>
   </main>
   <main v-else class="flex-auto flex flex-col relative">
@@ -92,8 +102,10 @@ import AppSettings from "./views/AppSettings.vue";
 import "./assets/css/index.css";
 import { useGlobalSetup } from "./assets/js/store/globalSetup";
 import DownloadDialog from '@/components/DownloadDialog.vue';
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible'
 
-const loading = ref(true);
+
+const isOpen = ref(false);
 
 const i18n = useI18N();
 
