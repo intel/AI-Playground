263f437a44da670fa7d933d07121a2177c5695f8
Markus Schuettler <markus.schuettler@tngtech.com>
2024-08-20 14:59:08 +0200

    Add shadcn-vue for resolution picker and error display (#2, #8)

--- WebUI/tailwind.config.cjs
+++ WebUI/tailwind.config.cjs
@@ -1,18 +1,26 @@
-// const plugin = require("tailwindcss/plugin");
-// const {
-//     iconsPlugin,
-//     getIconCollections,
-// } = require("@egoist/tailwindcss-icons");
+const animate = require("tailwindcss-animate")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  plugins: [
-    // iconsPlugin({
-    //     // Select the icon collections you want to use
-    //     collections: getIconCollections(["mdi"]),
-    // }),
-  ],
+  darkMode: ["class"],
+  safelist: ["dark"],
+  prefix: "",
+  
+  content: [
+    './pages/**/*.{ts,tsx,vue}',
+    './components/**/*.{ts,tsx,vue}',
+    './app/**/*.{ts,tsx,vue}',
+    './src/**/*.{ts,tsx,vue}',
+	],
+  
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         "color-bg-main": "var(--color-bg-main)",
@@ -24,7 +32,40 @@ module.exports = {
         "color-image-tool-button": "var(--color-image-tool-button)",
         "color-image-bg":"var(--color-image-bg)",
         "color-active":"var(--color-active)",
-        "color-control-bg":"var(--color-control-bg)"
+        "color-control-bg":"var(--color-control-bg)",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
       width: {
         "400px": "400px",
@@ -54,6 +95,37 @@ module.exports = {
         "768px": "768px",
         "1024px": "1024px",
       },
+      borderRadius: {
+      	xl: "calc(var(--radius) + 4px)",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+        "collapsible-down": {
+          from: { height: 0 },
+          to: { height: 'var(--radix-collapsible-content-height)' },
+        },
+        "collapsible-up": {
+          from: { height: 'var(--radix-collapsible-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "collapsible-down": "collapsible-down 0.2s ease-in-out",
+        "collapsible-up": "collapsible-up 0.2s ease-in-out",
+      },
     },
   },
-};
+  plugins: [animate],
+}
--- WebUI/vite.config.ts
+++ WebUI/vite.config.ts
@@ -4,6 +4,8 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite';
 import electron from "vite-plugin-electron";
 import pkg from "./package.json";
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 const assetParrentArray = [
   { parttern: /.(jpe?g|a?png)$/, folder: "image" },
@@ -33,6 +35,11 @@ export default defineConfig(({ command }) => {
   const sourcemap = isServe || !!process.env.VSCODE_DEBUG;
   const dependenciesToBeTranspiled = ['get-port'];
   return {
+    css: {
+      postcss: {
+        plugins: [tailwind(), autoprefixer()],
+      },
+    },
     plugins: [
       vue(),
       AutoImport({
