69731fb03fc0abf5bfc94d087c25bc8ad31c230b
marijnvg-tng <marijn.vangeest@tngtech.com>
2024-12-04 15:43:48 +0100

    Provided fronend interface for switching to ComfyUI workflow and checking availability.
    Signed-off-by: marijnvg-tng <marijn.vangeest@tngtech.com>

--- WebUI/src/components/SettingsImageWorkflowSelector.vue
+++ WebUI/src/components/SettingsImageWorkflowSelector.vue
@@ -19,42 +19,59 @@
        </div>
        </form>
    </dialog>
+  <dialog ref="ComfyUIConfirmationDialog" class="bg-gray-600 max-w-md p-7 items-center justify-center rounded-lg shadow-lg  text-white">
+    <form method="dialog" action="/submit" onsubmit="return false" class="items-center justify-center">
+      <p class="mb-4">
+        {{ languages.SETTINGS_MODEL_WORKFLOW_COMFYUI_CONFIRM }}
+      </p>
+      <div class="flex justify-between space-x-4 items-center">
+        <button @click="() => {cancel()}" type="submit" class="bg-slate-700 py-1 px-4 rounded">
+          {{ languages.COM_CANCEL }}
+        </button>
+        <div class="flex-end space-x-4">
+          <button @click="() => {comfyUIWarningOverride = true; currentWorkflow = 'comfyui'}" type="submit" class="bg-color-active py-1 px-4 rounded">
+            {{ languages.COM_CONFIRM }}
+          </button>
+        </div>
+      </div>
+    </form>
+  </dialog>
     <div class="items-center flex-wrap grid grid-cols-1 gap-2">
         <div class="flex flex-col gap-2">
             <p>{{ "Mode" }}</p>
             <div class="grid grid-cols-2 items-center gap-2 flex-wrap">
-                <radio-bolck :checked="imageGeneration.backend === 'default'"
+                <radio-block :checked="imageGeneration.backend === 'default'"
                     :text="'Default'"
-                    @click="() => { imageGeneration.backend = 'default' }"></radio-bolck>
-                <radio-bolck :checked="imageGeneration.backend === 'comfyui'"
+                    @click="() => { imageGeneration.backend = 'default' }"></radio-block>
+                <radio-block :checked="imageGeneration.backend === 'comfyui'"
                     :text="'Workflow'"
-                    @click="() => { imageGeneration.backend = 'comfyui' }"></radio-bolck>
+                    @click="() => { currentWorkflow = 'comfyui' }"></radio-block>
             </div>
         </div>
         <div v-if="imageGeneration.backend === 'default'" class="flex flex-col gap-2">
             <p>{{ languages.SETTINGS_MODEL_IMAGE_RESOLUTION }}</p>
             <div class="grid grid-cols-3 items-center gap-2 flex-wrap">
-                <radio-bolck :checked="classicModel === 'sd1.5'"
+                <radio-block :checked="classicModel === 'sd1.5'"
                     :text="languages.SETTINGS_MODEL_IMAGE_RESOLUTION_STRANDARD"
-                    @click="() => { classicModel = 'sd1.5' }"></radio-bolck>
-                <radio-bolck :checked="classicModel === 'sdxl'"
+                    @click="() => { classicModel = 'sd1.5' }"></radio-block>
+                <radio-block :checked="classicModel === 'sdxl'"
                     :text="languages.SETTINGS_MODEL_IMAGE_RESOLUTION_HD"
-                    @click="() => { classicModel = 'sdxl' }"></radio-bolck>
-                <radio-bolck :checked="classicModel === 'manual'"
+                    @click="() => { classicModel = 'sdxl' }"></radio-block>
+                <radio-block :checked="classicModel === 'manual'"
                     :text="languages.SETTINGS_MODEL_QUALITY_MANUAL"
-                    @click="() => { classicModel = 'manual' }"></radio-bolck>
+                    @click="() => { classicModel = 'manual' }"></radio-block>
             </div>
         </div>
         <div v-if="imageGeneration.backend === 'default'" class="flex flex-col gap-2">
             <p>{{ languages.SETTINGS_MODEL_QUALITY }}</p>
             <div class="grid grid-cols-3 items-center gap-2 flex-wrap">
-                <radio-bolck :checked="classicQuality === 'standard'"
+                <radio-block :checked="classicQuality === 'standard'"
                     :text="languages.SETTINGS_MODEL_QUALITY_STANDARD" @click="() => { classicQuality = 'standard' }"
-                    :disabled="classicModel === 'manual'"></radio-bolck>
-                <radio-bolck :checked="classicQuality === 'hq'" :text="languages.SETTINGS_MODEL_QUALITY_HIGH"
-                    @click="() => { classicQuality = 'hq' }" :disabled="classicModel === 'manual'"></radio-bolck>
-                <radio-bolck :checked="classicQuality === 'fast'" :text="languages.SETTINGS_MODEL_QUALITY_FAST"
-                    @click="() => { classicQuality = 'fast' }" :disabled="classicModel === 'manual'"></radio-bolck>
+                    :disabled="classicModel === 'manual'"></radio-block>
+                <radio-block :checked="classicQuality === 'hq'" :text="languages.SETTINGS_MODEL_QUALITY_HIGH"
+                    @click="() => { classicQuality = 'hq' }" :disabled="classicModel === 'manual'"></radio-block>
+                <radio-block :checked="classicQuality === 'fast'" :text="languages.SETTINGS_MODEL_QUALITY_FAST"
+                    @click="() => { classicQuality = 'fast' }" :disabled="classicModel === 'manual'"></radio-block>
             </div>
         </div>
         <div v-if="imageGeneration.backend === 'comfyui'" class="flex flex-col gap-2">
@@ -94,12 +111,17 @@
 <script setup lang="ts">
 import { useImageGeneration } from "@/assets/js/store/imageGeneration";
 import DropSelector from "../components/DropSelector.vue";
-import RadioBolck from "../components/RadioBlock.vue";
+import RadioBlock from "../components/RadioBlock.vue";
+import {useGlobalSetup} from "@/assets/js/store/globalSetup.ts";
 
 const imageGeneration = useImageGeneration();
+const globalSetup = useGlobalSetup();
 
 const hdConfirmationDialog = ref<HTMLDialogElement>();
 const hdWarningOverride = ref(false);
+
+const ComfyUIConfirmationDialog = ref<HTMLDialogElement>();
+const comfyUIWarningOverride = ref(false);
 
 const classicModel = computed({
         get() {
@@ -129,6 +151,57 @@ const classicModel = computed({
             imageGeneration.activeWorkflowName = targetWorkflow;
         }
       })
+
+const currentWorkflow = computed({
+        get() {
+          if (imageGeneration.backend === 'default') {
+            return 'default'
+          }
+          if (imageGeneration.backend === 'comfyui') {
+            return 'comfyui'
+          }
+          return 'default'
+        },
+        set(newValue) {
+          if (newValue === 'default') {
+            imageGeneration.backend = 'default'
+            return;
+          }
+          if (newValue === 'comfyui') {
+            const comfyUIInstalled = isComfyUIDownloaded();
+
+            if (!comfyUIInstalled && !comfyUIWarningOverride.value) {
+              ComfyUIConfirmationDialog.value?.showModal();
+              return;
+            } else if (comfyUIInstalled) {
+              // Comfy installed
+              comfyUIWarningOverride.value = false;
+            } else {
+              // Confirm
+              // comfyUIWarningOverride.value = false;
+              triggerInstallComfyUI()
+              imageGeneration.backend = 'comfyui'
+            }
+
+          }
+        }
+})
+
+function isComfyUIDownloaded(){
+    const response = fetch(`${globalSetup.apiHost}/api/comfy-ui/is_installed`);
+    return response.;
+}
+
+function triggerInstallComfyUI(){
+    const response = fetch(`${globalSetup.apiHost}/api/`);
+    return true;
+}
+
+function cancel(){
+  // ToDo Cancel everything and delete everything!!
+  ComfyUIConfirmationDialog.value?.close();
+}
+
 const classicQuality = computed({
         get() {
           if (!imageGeneration.activeWorkflowName?.match(/(Standard|HD)/)) {
