b84ccce15de8e65a5e524d0f5da6626e79138fa9
Markus Schuettler <markus.schuettler@tngtech.com>
2024-12-13 16:56:41 +0100

    Assert dynamic input types

--- WebUI/src/components/SettingsImageComfyDynamic.vue
+++ WebUI/src/components/SettingsImageComfyDynamic.vue
@@ -3,16 +3,16 @@
         <p>{{ input.label }}</p>
 
         <!--    Number    -->
-        <slide-bar v-if="input.type === 'number'" v-model:current="input.current.value" :min="input.min"
+        <slide-bar v-if="input.type === 'number'" v-model:current="(input.current.value as number)" :min="input.min"
             :max="input.max" :step="input.step"></slide-bar>
 
         <!--    Image    -->
-        <img ref="imgDropZones" v-if="input.type === 'image'" :src="input.current.value" alt="Image" class="w-64 h-64 object-scale-down self-center"></img>
+        <img ref="imgDropZones" v-if="input.type === 'image'" :src="(input.current.value as string)" alt="Image" class="w-64 h-64 object-scale-down self-center"></img>
         <Input v-if="input.type === 'image'" accept="image/jpeg,image/png,image/webp" id="picture" type="file"
-            v-on:change="(e) => handleFilesEvent(input.current)(e)"></Input>
+            v-on:change="(e: Event) => handleFilesEvent(input.current as Ref<string, string>)(e)"></Input>
 
         <!--    String    -->
-        <Input v-if="input.type === 'string'" type="text" v-model="input.current.value"></Input>
+        <Input v-if="input.type === 'string'" type="text" v-model="(input.current.value as string)"></Input>
 
         <!--    Boolean    -->
         <button v-if="input.type === 'boolean'" class="v-checkbox-control flex-none w-5 h-5"
