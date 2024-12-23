57e25388697eebf10031a2a27e6f69247326b429
Nuullll <vfirst218@gmail.com>
2024-12-23 21:58:03 +0800

    remove apiService.ts

--- WebUI/electron/subprocesses/apiService.ts
+++
@@ -1,6 +0,0 @@
-import {ChildProcess} from "node:child_process";
-import path from "node:path";
-import {app, BrowserWindow} from "electron";
-import {appLoggerInstance} from "../logging/logger.ts";
-import fs from "fs";
-import * as filesystem from "fs-extra";
