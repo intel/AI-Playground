f03ae870fa1ae1f2d2741105c15077362701b6f6
Nicolas Oliver <dario.n.oliver@intel.com>
2025-02-16 13:05:50 -0800

    Show progress bars for metrics

--- WebUI/src/components/SettingsMetrics.vue
+++ WebUI/src/components/SettingsMetrics.vue
@@ -3,43 +3,77 @@
     <!-- Add a button to fetch metrics -->
     <div class="flex-none flex flex-row justify-between items-center px-3 py-2 border-b border-color-spilter">
       <h1 class="text-lg font-bold">Metrics</h1>
-      <button class="btn btn-primary" @click="fetchMetrics">Fetch Metrics</button>
-    </div>
-    <div v-if="metrics" class="metrics-container">
-      <h2>Metrics</h2>
-      <pre>{{ metrics }}</pre>
     </div>
     <div v-if="cpuTotalUsagePercentage" class="metrics-container">
-      CPU: {{ cpuTotalUsagePercentage[cpuTotalUsagePercentage.length - 1].value }}%
+      <div class="flex flex-row justify-between items-center w-full">
+        <label for="cpu-usage" class="w-1/4">{{ metricsLabels.cpuUsage }}</label>
+        <progress id="cpu-usage" :value="getLastValue(cpuTotalUsagePercentage)" max="100" class="w-3/4">
+          {{ getLastValue(cpuTotalUsagePercentage) }}%
+        </progress>
+      </div>
     </div>
     <div v-if="memoryTotalUsagePercentage" class="metrics-container">
-      Memory: {{ memoryTotalUsagePercentage[memoryTotalUsagePercentage.length - 1].value }}%
+      <div class="flex flex-row justify-between items-center w-full">
+        <label for="memory-usage" class="w-1/4">{{ metricsLabels.memoryUsage }}</label>
+        <progress id="memory-usage" :value="getLastValue(memoryTotalUsagePercentage)" max="100" class="w-3/4">
+          {{ memoryTotalUsagePercentage }}%
+        </progress>
+      </div>
     </div>
     <div v-if="gpuDedicatedMemoryPercentage" class="metrics-container">
-      GPU Dedicated Memory: {{ gpuDedicatedMemoryPercentage[gpuDedicatedMemoryPercentage.length - 1].value }}%
+      <div class="flex flex-row justify-between items-center w-full">
+        <label for="gpu-dedicated-memory" class="w-1/4">{{ metricsLabels.gpuDedicatedMemoryUsage }}</label>
+        <progress id="gpu-dedicated-memory" :value="getLastValue(gpuDedicatedMemoryPercentage)" max="100" class="w-3/4">
+          {{ getLastValue(gpuDedicatedMemoryPercentage) }}%
+        </progress>
+      </div>
     </div>
     <div v-if="gpuCopyUsagePercentage" class="metrics-container">
-      GPU Copy Usage: {{ gpuCopyUsagePercentage[gpuCopyUsagePercentage.length - 1].value }}%
+      <div class="flex flex-row justify-between items-center w-full">
+        <label for="gpu-copy-usage" class="w-1/4">{{ metricsLabels.gpuCopyUsage }}</label>
+        <progress id="gpu-copy-usage" :value="getLastValue(gpuCopyUsagePercentage)" max="100" class="w-3/4">
+          {{ getLastValue(gpuCopyUsagePercentage) }}%
+        </progress>
+      </div>
     </div>
     <div v-if="gpuComputeUsagePercentage" class="metrics-container">
-      GPU Compute Usage: {{ gpuComputeUsagePercentage[gpuComputeUsagePercentage.length - 1].value }}%
+      <div class="flex flex-row justify-between items-center w-full">
+        <label for="gpu-compute-usage" class="w-1/4">{{ metricsLabels.gpuComputeUsage }}</label>
+        <progress id="gpu-compute-usage" :value="getLastValue(gpuComputeUsagePercentage)" max="100" class="w-3/4">
+          {{ getLastValue(gpuComputeUsagePercentage) }}%
+        </progress>
+      </div>
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
 import { useI18N } from '@/assets/js/store/i18n.ts'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 import { useGlobalSetup } from '@/assets/js/store/globalSetup.ts'
 
 const globalSetup = useGlobalSetup()
 useI18N()
 
 const metrics = ref(null)
+
+const metricsLabels = {
+  cpuUsage: 'CPU Utilization',
+  memoryUsage: 'Memory Utilization',
+  gpuDedicatedMemoryUsage: 'GPU Dedicated Memory Utilization',
+  gpuCopyUsage: 'GPU Copy Engine Utilization',
+  gpuComputeUsage: 'GPU Compute Engine Utilization',
+}
+
 const cpuTotalUsagePercentage = ref(null)
 const memoryTotalUsagePercentage = ref(null)
 const gpuDedicatedMemoryPercentage = ref(null)
 const gpuCopyUsagePercentage = ref(null)
 const gpuComputeUsagePercentage = ref(null)
+
+const getLastValue = (metric) => {
+  return metric[metric.length - 1].value
+}
 
 const fetchMetrics = async () => {
   try {
@@ -49,6 +83,7 @@ const fetchMetrics = async () => {
     }
     metrics.value = await response.json()
 
+    // Timeseries data
     cpuTotalUsagePercentage.value = await (await fetch(`${globalSetup.apiHost}/api/metrics/CPUTotalUsagePercentage`)).json()
     memoryTotalUsagePercentage.value = await (await fetch(`${globalSetup.apiHost}/api/metrics/MemoryTotalUsagePercentage`)).json()
     gpuDedicatedMemoryPercentage.value = await (await fetch(`${globalSetup.apiHost}/api/metrics/GPUDedicatedMemoryPercentage`)).json()
@@ -62,5 +97,16 @@ const fetchMetrics = async () => {
 
 onMounted(() => {
   fetchMetrics()
+  const intervalId = setInterval(fetchMetrics, 3000)
+  onUnmounted(() => {
+    clearInterval(intervalId)
+  })
 })
 </script>
+
+<style scoped>
+.metrics-container {
+  margin-top: 10px;
+}
+
+</style>
