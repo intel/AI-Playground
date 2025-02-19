a28a31ffff2ee55baeddeb887088ad7f082b0859
Julian Bollig <julian.bollig@tngtech.com>
2025-02-19 18:39:26 +0000

    feat: update OpenVINO integration in build scripts and requirements (#50)

--- WebUI/electron/subprocesses/openVINOBackendService.ts
+++ WebUI/electron/subprocesses/openVINOBackendService.ts
@@ -55,6 +55,12 @@ export class OpenVINOBackendService extends LongLivedPythonApiService {
       }
       const commonRequirements = existingFileOrError(path.join(self.serviceDir, 'requirements.txt'))
       await this.uvPip.run(['install', '-r', commonRequirements])
+      await this.uvPip.run([
+        'install',
+        '--extra-index-url', 'https://storage.openvinotoolkit.org/simple/wheels/nightly',
+        'openvino-genai==2025.1.0.0.dev20250203'
+      ]);
+      
       yield {
         serviceName: self.name,
         step: `install dependencies`,
