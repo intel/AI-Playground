ec7e9b9c9214450d666b8d0d3053f7c515e2e604
Daniel Hirsch <daniel.hirsch@tngtech.com>
2025-03-10 19:46:14 +0100

    Update requirements

--- OpenVINO/openvino_embeddings.py
+++ OpenVINO/openvino_embeddings.py
@@ -3,23 +3,13 @@ from langchain_community.embeddings import OpenVINOEmbeddings
 
 class OpenVINOEmbeddingModel:
     def __init__(self):
-        self.embedding_model_id = "default_embedding_model"
-        self.embedding_device = "CPU" # or "GPUT"
-        self.embedding_model_configuration = {
-            "mean_pooling": True,
-            "normalize_embeddings": True
-        }
-
-        embedding_model_kwargs = {"device": self.embedding_device, "compile": False}
+        model_name = "BAAI/bge-large-en-v1.5"
+        model_kwargs = {'device': 'GPU'}
         encode_kwargs = {
-            "mean_pooling": self.embedding_model_configuration["mean_pooling"],
-            "normalize_embeddings": self.embedding_model_configuration["normalize_embeddings"],
+            "mean_pooling": True,
+            "normalize_embeddings": True,
             "batch_size": 4,
         }
-
-        model_name = "sentence-transformers/all-mpnet-base-v2"
-        model_kwargs = {'device': 'CPU'}
-        encode_kwargs = {'normalize_embeddings': True}
         self.embedding = OpenVINOEmbeddings(
             model_name_or_path=model_name,
             model_kwargs=model_kwargs,
