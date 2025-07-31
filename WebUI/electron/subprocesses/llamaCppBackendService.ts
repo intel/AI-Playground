252023217f8f9beae85a62cc473f6ce8fc32a593
Markus Schuettler <markus.schuettler@tngtech.com>
2025-07-31 20:47:24 +0100

    chore: upgrade llama-cpp version

--- WebUI/electron/subprocesses/llamaCppBackendService.ts
+++ WebUI/electron/subprocesses/llamaCppBackendService.ts
@@ -26,7 +26,7 @@ export class LlamaCppBackendService extends LongLivedPythonApiService {
   devices: InferenceDevice[] = [{ id: 'AUTO', name: 'Auto select device', selected: true }]
   readonly isRequired = false
 
-  private version = 'b5836'
+  private version = 'b6050'
 
   healthEndpointUrl = `${this.baseUrl}/health`
 
