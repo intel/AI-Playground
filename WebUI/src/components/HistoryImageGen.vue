a9c846c7803bcc94ea548722545434cb7ae14b93
Timo Wuttke <timo.wuttke@tngtech.com>
2025-10-23 14:38:15 +0100

    remove duplicated watch block

--- WebUI/src/components/HistoryImageGen.vue
+++ WebUI/src/components/HistoryImageGen.vue
@@ -1,10 +1,10 @@
 <template>
   <div
-    v-show="imageGeneration.generatedImages.filter((i) => i.state !== 'queued').length > 0"
+    v-show="nonQueuedImages.length > 0"
     class="flex flex-col space-y-2 p-4 bg-neutral-900 h-full overflow-y-auto"
   >
     <div
-      v-for="image in imageGeneration.generatedImages.filter((i) => i.state !== 'queued')"
+      v-for="image in nonQueuedImages"
       :key="image.id"
       class="flex items-center gap-2 bg-gray-700 rounded p-1 cursor-pointer relative border-2 transition-colors hover:bg-gray-600"
       :class="imageGeneration.selectedGeneratedImageId === image.id ? 'border-blue-500' : 'border-transparent'"
@@ -55,16 +55,13 @@
       </div>
     </div>
   </div>
-  <div
-    v-if="imageGeneration.generatedImages.filter((i) => i.state !== 'queued').length === 0"
-    class="text-gray-400 text-center p-5 italic"
-  >
+  <div v-else class="text-gray-400 text-center p-5 italic">
     No images generated yet.
   </div>
 </template>
 
 <script setup lang="ts">
-import { watch } from 'vue'
+import { computed } from 'vue'
 import { useI18N } from '@/assets/js/store/i18n'
 import * as toast from '@/assets/js/toast'
 import { MediaItem, isVideo, useImageGeneration } from '@/assets/js/store/imageGeneration'
@@ -90,24 +87,16 @@ import { Button } from '@/components/ui/button'
 const imageGeneration = useImageGeneration()
 const i18nState = useI18N().state
 
+const nonQueuedImages = computed(() =>
+  imageGeneration.generatedImages.filter((i) => i.state !== 'queued')
+)
+
 const dragImage = (item: MediaItem | null) => (event: DragEvent) => {
   if (!item) return
   event.preventDefault()
   const url = isVideo(item) ? item.videoUrl : item.imageUrl
   window.electronAPI.startDrag(url)
 }
-
-watch(
-  () => imageGeneration.generatedImages.filter((i) => i.state !== 'queued').length,
-  () => {
-    const nonQueuedImages = imageGeneration.generatedImages.filter((i) => i.state !== 'queued')
-    if (nonQueuedImages.length > 0) {
-      imageGeneration.selectedGeneratedImageId = nonQueuedImages[nonQueuedImages.length - 1].id
-    } else {
-      imageGeneration.selectedGeneratedImageId = null
-    }
-  },
-)
 
 function reloadImage(image: MediaItem) {
   console.log('Reloading image:', image.id)
