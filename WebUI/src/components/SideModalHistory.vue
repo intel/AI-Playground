82c9a98a8796edbc14449a54381b550ab788c347
Lukas Baron <lukas.baron@tngtech.com>
2026-02-05 14:02:34 +0100

    Tooltips for the add, close, delete-all buttons
    Signed-off-by: Lukas Baron <lukas.baron@tngtech.com>

--- WebUI/src/components/SideModalHistory.vue
+++ WebUI/src/components/SideModalHistory.vue
@@ -8,7 +8,7 @@
     <template #header-buttons>
       <AlertDialog v-if="mode !== 'chat'">
         <AlertDialogTrigger asChild>
-          <button class="svg-icon i-clear w-6 h-6" />
+          <button class="svg-icon i-clear w-6 h-6" :title="languages.COM_CLEAR_HISTORY" />
         </AlertDialogTrigger>
         <AlertDialogContent>
           <AlertDialogHeader>
@@ -31,11 +31,13 @@
         v-show="mode === 'chat'"
         @click="selectNewConversation"
         class="svg-icon i-add w-7 h-7"
+        :title="languages.COM_ADD"
       />
       <button
         v-show="mode === 'imageGen' || mode === 'imageEdit' || mode === 'video'"
         @click="selectNewMedia"
         class="svg-icon i-add w-7 h-7"
+        :title="languages.COM_ADD"
       />
     </template>
 
